ff22571e85a775013c4ac9933b6dcf720c84341a
LennardN <lennard.norden.ln@gmail.com>
2021-03-03 14:25:09 +0100

    Add project files.

---
+++ FunktionsRechner/FunktionsRechner.h
@@ -0,0 +1,32 @@
+
+// FunktionsRechner.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CFunktionsRechnerApp:
+// See FunktionsRechner.cpp for the implementation of this class
+//
+
+class CFunktionsRechnerApp : public CWinApp
+{
+public:
+	CFunktionsRechnerApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFunktionsRechnerApp theApp;
