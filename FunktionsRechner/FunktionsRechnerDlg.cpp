edee076c64442e78e956c96544d2087f66dd068a
LennardN <lennard.norden.ln@gmail.com>
2021-06-09 13:17:27 +0200

    2. Ableitung hinzugefügt

--- FunktionsRechner/FunktionsRechnerDlg.cpp
+++ FunktionsRechner/FunktionsRechnerDlg.cpp
@@ -184,6 +184,7 @@ void CFunktionsRechnerDlg::OnBnClickedOk()
 
 	UpdateData(TRUE);
 	if (m_cbCheck1.GetCheck() == true) {
+		//ABleitung 1
 		int x1;
 		CString xs1;
 		CString pre1x(_T("NoValue/IncorrectValue"));
@@ -199,6 +200,23 @@ void CFunktionsRechnerDlg::OnBnClickedOk()
 		pre1x.Format(L"%d", pre1xi);
 		post1x.Format(L"%d", post1xi);
 		m_csErg = pre1x + "x^" + post1x;
+		CString erg1 = pre1x + "x^" + post1x;
+		//Ableitung 2
+		int x12;
+		CString xs12;
+		CString pre12x(_T("NoValue/IncorrectValue"));
+		CString post12x(_T("NoValue/IncorrectValue"));
+		x12 = erg1.Find('x');
+		xs12.Format(L"%d", x12);
+		pre12x = erg1.Left(x12);
+		post12x = erg1.Right(erg1.GetLength() - x12 - 2);
+		int pre12xi = _wtoi(pre12x);
+		int post12xi = _wtoi(post12x);
+		pre12xi = post12xi * pre12xi;
+		post12xi = post12xi - 1;
+		pre12x.Format(L"%d", pre12xi);
+		post12x.Format(L"%d", post12xi);
+		m_csERG1 = pre12x + "x^" + post12x;
 		if (m_cbCheck2.GetCheck() == true) {
 			int x2;
 			CString xs2;
@@ -215,6 +233,23 @@ void CFunktionsRechnerDlg::OnBnClickedOk()
 			pre2x.Format(L"%d", pre2xi);
 			post2x.Format(L"%d", post2xi);
 			m_csErg = pre1x + "x^" + post1x + " + " + pre2x + "x^" + post2x;
+			CString erg2 = pre2x + "x^" + post2x;
+			//Ableitung 2
+			int x22;
+			CString xs22;
+			CString pre22x(_T("NoValue/IncorrectValue"));
+			CString post22x(_T("NoValue/IncorrectValue"));
+			x22 = erg2.Find('x');
+			xs22.Format(L"%d", x22);
+			pre22x = erg2.Left(x22);
+			post22x = erg2.Right(erg2.GetLength() - x22 - 2);
+			int pre22xi = _wtoi(pre22x);
+			int post22xi = _wtoi(post22x);
+			pre22xi = post22xi * pre22xi;
+			post22xi = post22xi - 1;
+			pre22x.Format(L"%d", pre22xi);
+			post22x.Format(L"%d", post22xi);
+			m_csERG1 = pre12x + "x^" + post12x + " + " + pre22x + "x^" + post22x;
 			if (m_cbCheck3.GetCheck() == true) {
 				int x3;
 				CString xs3;
@@ -231,6 +266,23 @@ void CFunktionsRechnerDlg::OnBnClickedOk()
 				pre3x.Format(L"%d", pre3xi);
 				post3x.Format(L"%d", post3xi);
 				m_csErg = pre1x + "x^" + post1x + " + " + pre2x + "x^" + post2x + " + " + pre3x + "x^" + post3x;
+				CString erg3 = pre3x + "x^" + post3x;
+				//Ableitung 2
+				int x32;
+				CString xs32;
+				CString pre32x(_T("NoValue/IncorrectValue"));
+				CString post32x(_T("NoValue/IncorrectValue"));
+				x32 = erg3.Find('x');
+				xs32.Format(L"%d", x32);
+				pre32x = erg3.Left(x32);
+				post32x = erg3.Right(erg3.GetLength() - x32 - 2);
+				int pre32xi = _wtoi(pre32x);
+				int post32xi = _wtoi(post32x);
+				pre32xi = post32xi * pre32xi;
+				post32xi = post32xi - 1;
+				pre32x.Format(L"%d", pre32xi);
+				post32x.Format(L"%d", post32xi);
+				m_csERG1 = pre12x + "x^" + post12x + " + " + pre22x + "x^" + post22x + " + " + pre32x + "x^" + post32x;
 				if (m_cbCheck4.GetCheck() == true) {
 					int x4;
 					CString xs4;
@@ -247,6 +299,23 @@ void CFunktionsRechnerDlg::OnBnClickedOk()
 					pre4x.Format(L"%d", pre4xi);
 					post4x.Format(L"%d", post4xi);
 					m_csErg = pre1x + "x^" + post1x + " + " + pre2x + "x^" + post2x + " + " + pre3x + "x^" + post3x + " + " + pre4x + "x^" + post4x;
+					CString erg4 = pre4x + "x^" + post4x;
+					//Ableitung 2
+					int x42;
+					CString xs42;
+					CString pre42x(_T("NoValue/IncorrectValue"));
+					CString post42x(_T("NoValue/IncorrectValue"));
+					x42 = erg4.Find('x');
+					xs42.Format(L"%d", x42);
+					pre42x = erg4.Left(x42);
+					post42x = erg4.Right(erg4.GetLength() - x42 - 2);
+					int pre42xi = _wtoi(pre42x);
+					int post42xi = _wtoi(post42x);
+					pre42xi = post42xi * pre42xi;
+					post42xi = post42xi - 1;
+					pre42x.Format(L"%d", pre42xi);
+					post42x.Format(L"%d", post42xi);
+					m_csERG1 = pre12x + "x^" + post12x + " + " + pre22x + "x^" + post22x + " + " + pre32x + "x^" + post32x + " + " + pre42x + "x^" + post42x;
 				}
 			}
 		}
